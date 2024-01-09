a8e2beac2945e07596a58b018cf70cf38d53504f
Arthur de Moulins <arthurdemoulins@gmail.com>
2024-01-09 17:45:13 +0100

    add user/group resolution

--- Repository/UserRepositoryInterface.php
+++ Repository/UserRepositoryInterface.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace Alchemy\AclBundle\Repository;
 
+use Alchemy\AclBundle\Model\AclUserInterface;
+
 interface UserRepositoryInterface
 {
     public function getUsers(int $limit, int $offset = 0): array;
 
     public function getUser(string $userId): ?array;
+
+    public function getAclGroupsId(AclUserInterface $user): array;
 }
